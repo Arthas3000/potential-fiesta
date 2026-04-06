67d5203bddf8e55372dd03d4b49932f14c74677f
Arthas3000 <lbguibes2010@gmail.com>
2026-04-06 10:32:45 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,52 +1,32 @@
-const CACHE_NAME = 'finance-app-v13';
-// Arrumando os caminhos para o GitHub Pages
+const CACHE_NAME = 'finance-app-v16';
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-192x192.png',
-  './icons/icon-512x512.png',
   'https://cdn.tailwindcss.com',
   'https://cdn.jsdelivr.net/npm/chart.js',
-  'https://unpkg.com/lucide@latest' // Adicionado Lucide Icons
+  'https://unpkg.com/lucide@latest'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-      .catch(err => console.log('Erro ao salvar no cache:', err))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
   self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-      .catch(() => {
-        // Se a internet cair de vez, sempre tenta voltar pro index
-        return caches.match('./index.html');
-      })
+    caches.match(event.request).then(response => response || fetch(event.request))
+      .catch(() => caches.match('./index.html'))
   );
 });
 
 self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    caches.keys().then(cacheNames => Promise.all(
+      cacheNames.map(cacheName => {
+        if (cacheName !== CACHE_NAME) return caches.delete(cacheName);
+      })
+    ))
   );
   self.clients.claim();
 });
